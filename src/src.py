6aef4426baba6b84b293cb6f797b20f396afe7c8
marble <msm.mirdamad@gmail.com>
2026-05-23 23:47:30 +0330

    drop Gender and Age column

--- src/src.py
+++ src/src.py
@@ -12,3 +12,7 @@ df = df.drop('CustomerID', axis=1)
 sns.pairplot(df, hue='Gender')
 plt.suptitle("Pairplot segmented by Gender", y=1.02)
 plt.show()
+
+# drop Gender & Age column because it has no influence on clustering
+df = df.drop('Gender', axis=1)
+df = df.drop('Age', axis=1)
